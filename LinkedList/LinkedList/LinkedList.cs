411fff46a55aae4d84cae3551645903bd1ada9cb
Walter M. Soto <wmsoto@hotmail.com>
2015-08-27 10:04:35 -0400

    Singly linked list sample

---
+++ LinkedList/LinkedList/LinkedList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace LinkedList
+{
+    public class Node<T>
+    {
+        public T Data { set; get; }
+        public Node<T> Next { set; get; } 
+    }
+
+    public class LinkedList<T>
+    {
+
+        private Node<T> root;
+        private Node<T> current;
+
+        public void Add(T data)
+        {
+            var node = new Node<T>
+            {
+                Data = data
+            };
+
+            if (root == null)
+            {
+                root = node;
+            }
+            else
+            { 
+                current.Next = node; 
+            }
+            current = node;
+        }
+
+        public void Traverse(Action<T> callback)
+        {
+            Node<T> selected = root;
+            while (selected != null)
+            {
+                if (callback != null)
+                {
+                    callback(selected.Data);
+                }
+                selected = selected.Next;
+            }
+        }
+
+
+
+    }
+}
---
+++ LinkedList/SinglyLinkedListTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System; 
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinkedList;
+
+namespace SinglyLinkedListTest
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            var list = new LinkedList<string>();
+
+            list.Add("A");
+            list.Add("B");
+            list.Add("C");
+
+            list.Traverse(Console.WriteLine);
+
+
+            Console.WriteLine("Hit any key...");
+            Console.ReadLine();
+        }
+    }
+}
